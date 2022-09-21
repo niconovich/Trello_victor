85b8430b4df679f46b01a78ca1644f895cc1bedd
Viktor Niconovich <niconovich@gmail.com>
2022-09-21 13:43:20 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,21 @@ import { Modal } from 'bootstrap';
 import { $ } from './helpers.js';
 import { clock } from './clock.js';
 import { users } from './jsonplaceholder.js'
+
+
 setInterval(clock, 1000);
 clock();
 
 
 console.log(users())
+
+//modal.show()
+let AddEllement=document.querySelector('#addTodo')
+AddEllement.addEventListener('click',function(event) {
+    let preventDefault=event.preventDefault();
+    console.log(event)
+    let modalElem =  document.querySelector('.modalAdd')
+    console.log(modalElem)
+    modalElem.classList.add('active');
+    overlay.classList.add('active');
+})
